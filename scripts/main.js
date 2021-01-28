bbca05daf6d5cc459779ef70ebe63689848ac874
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-01-28 19:36:59 +0300

    доработка проекта

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,29 @@
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form')
+let nameInput = formElement.querySelector('.popup__item_type_name')
+let jobInput = formElement.querySelector('.popup__item_type_description')
+let saveButton = formElement.querySelector('.popup__save-button')
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__description');
 
 function openPopup() {
     popup.classList.toggle('popup_opened');
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
 }
-
-popupOpenButton.addEventListener('click', openPopup);
-
-popupCloseButton.addEventListener('click', openPopup);
-
-let formElement = document.querySelector('.popup__form')
-
-let nameInput = formElement.querySelector('.popup__name')
-let jobInput = formElement.querySelector('.popup__description')
-let saveButton = formElement.querySelector('.popup__save-button')
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     nameInput.getAttribute('value');
     jobInput.getAttribute('value');
-
-    let nameProfile = document.querySelector('.profile__name');
-    let jobProfile = document.querySelector('.profile__description');
-
     nameProfile.textContent = nameInput.value
     jobProfile.textContent = jobInput.value
+    openPopup()
 }
-saveButton.addEventListener('click', openPopup);
+popupOpenButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', openPopup); 
 
 formElement.addEventListener('submit', formSubmitHandler);
