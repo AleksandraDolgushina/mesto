b2b97e092b92ebd694d43e3b2149c8d3ee98e031
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-02-21 21:32:08 +0300

    feat: активность кнопки

--- scripts/validate.js
+++ scripts/validate.js
@@ -20,15 +20,32 @@ const checkInputValidity = (formElement, inputElement) => {
     }
   };
 
+  const toggleButtonState = (inputList, buttonElement) => {
+    const findAtLeastOneNotValid = (inputElement) => !inputElement.validity.valid;
+    const hasNotValidInput = inputList.some(findAtLeastOneNotValid);
+  
+    if (hasNotValidInput) {
+      buttonElement.setAttribute("disabled", true);
+      buttonElement.classList.add("popup__save-button_inactive");
+    } else {
+      buttonElement.removeAttribute("disabled");
+      buttonElement.classList.remove("popup__save-button_inactive");
+    }
+  };
+
 const setEventListeners = (formElement) => {
     formElement.addEventListener('submit', (evt) => {
         evt.preventDefault();
       });
 
     const inputList = Array.from(formElement.querySelectorAll('.popup__item'));
+    const buttonElement = formElement.querySelector('.popup__save-button');
+    toggleButtonState(inputList, buttonElement)
+    
     inputList.forEach((inputElement) => {
-        inputElement.addEventListener('input', (evt) => {
+        inputElement.addEventListener('input', () => {
             checkInputValidity(formElement, inputElement)
+            toggleButtonState(inputList, buttonElement)
         })
     })
 }
